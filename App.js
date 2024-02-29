5dbf6df114b18524d76cec5814187d0914a5e816
Kuntjo <vitokuntjoro@gmail.com>
2024-02-29 12:08:19 +0700

    29 Feb 2024

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+
+//first of all after getting the firebase we eed routing
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        <Stack.Screen name='login' component={Login} options={{ headerShown:false }}></Stack.Screen>
+        <Stack.Screen name='home' component={Home} options={{ headerShown:false }}></Stack.Screen>
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
